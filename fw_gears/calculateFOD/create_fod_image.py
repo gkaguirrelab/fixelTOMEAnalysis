ea5b68101b802143d7c13876160918ded8f46e53
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2021-02-01 18:44:33 -0500

    fod script small modifications

--- fw_gears/calculateFOD/create_fod_image.py
+++ fw_gears/calculateFOD/create_fod_image.py
@@ -14,17 +14,17 @@ def create_fod_image(preprocessed_dti, mask, bvecs, bvals, response_wm, response
         response_wm = os.path.join(workdir, 'response_wm.txt')
         response_gm = os.path.join(workdir, 'response_gm.txt')    
         response_csf = os.path.join(workdir, 'response_csf.txt')    
-        os.system('%s dhollander %s -fslgrad %s %s %s %s %s -force -quiet' % (os.path.join(mrtrix_bin_path, 'dwi2response'),
-                                                                       preprocessed_dti, bvecs, bvals, 
-                                                                       response_wm, response_gm, response_csf))
+        os.system('%s dhollander %s -fslgrad %s %s %s %s %s -scratch %s' % (os.path.join(mrtrix_bin_path, 'dwi2response'),
+                                                                                preprocessed_dti, bvecs, bvals, 
+                                                                                response_wm, response_gm, response_csf, workdir))
     
     # Upsample DWI images and the mask to isotropic 1.25mm
     print('Upsampling DWI')
     upsampled_dti = os.path.join(workdir, 'upsampled_%s_dti.mif' % subject_id)
     upsampled_mask = os.path.join(workdir, 'upsampled_%s_mask.mif' % subject_id)
-    os.system('%s %s regrid -vox 1.25 %s -force -quiet' % (os.path.join(mrtrix_bin_path, 'mrgrid'),
+    os.system('%s %s regrid -vox 1.25 %s' % (os.path.join(mrtrix_bin_path, 'mrgrid'),
                                                     preprocessed_dti, upsampled_dti))
-    os.system('%s %s regrid -vox 1.25 %s -force -quiet' % (os.path.join(mrtrix_bin_path, 'mrgrid'),
+    os.system('%s %s regrid -vox 1.25 %s' % (os.path.join(mrtrix_bin_path, 'mrgrid'),
                                                     mask, upsampled_mask))
     
     # Create the FOD image
@@ -32,14 +32,14 @@ def create_fod_image(preprocessed_dti, mask, bvecs, bvals, response_wm, response
     wm_fod = os.path.join(workdir, 'wm_fod.mif')
     gm_fod = os.path.join(workdir, 'gm_fod.mif')
     csf_fod = os.path.join(workdir, 'csf_fod.mif')
-    os.system('%s msmt_csd %s %s %s %s %s %s %s -mask %s -lmax %s -force -fslgrad %s %s -quiet' % (os.path.join(mrtrix_bin_path, 'dwi2fod'), upsampled_dti,
-                                                                                                   response_wm, wm_fod, response_gm, gm_fod, response_csf, csf_fod,
-                                                                                                   upsampled_mask, lmax, bvecs, bvals))
+    os.system('%s msmt_csd %s %s %s %s %s %s %s -mask %s -lmax %s -fslgrad %s %s' % (os.path.join(mrtrix_bin_path, 'dwi2fod'), upsampled_dti,
+                                                                                     response_wm, wm_fod, response_gm, gm_fod, response_csf, csf_fod,
+                                                                                     upsampled_mask, lmax, bvecs, bvals))
     
     # Calculate a more conservetive mask from the upsampled data for the next
     # step (bias coorection and intensity normalization)
     normalize_mask = os.path.join(workdir, 'normalize_mask.mif')
-    os.system('%s %s %s -fslgrad %s %s -force -quiet' % (os.path.join(mrtrix_bin_path, 'dwi2mask'), upsampled_dti, normalize_mask, bvecs, bvals))
+    os.system('%s %s %s -fslgrad %s %s' % (os.path.join(mrtrix_bin_path, 'dwi2mask'), upsampled_dti, normalize_mask, bvecs, bvals))
     
     # Correct and intensity normalize the image. This step is suggested even
     # if a previous field correction was applied. It will coorect residual 
@@ -48,5 +48,5 @@ def create_fod_image(preprocessed_dti, mask, bvecs, bvals, response_wm, response
     normalized_wm_fod = os.path.join(output_dir, 'preproc_wm_fod_%s.nii.gz' % subject_id)
     normalized_gm_fod = os.path.join(output_dir, 'preproc_gm_fod_%s.nii.gz' % subject_id)
     normalized_csf_fod = os.path.join(output_dir, 'preproc_csf_fod_%s.nii.gz' % subject_id)
-    os.system('%s %s %s %s %s %s %s -mask %s -force -quiet' % (os.path.join(mrtrix_bin_path, 'mtnormalise'), wm_fod, normalized_wm_fod,
-                                                               gm_fod, normalized_gm_fod, csf_fod, normalized_csf_fod, normalize_mask))
+    os.system('%s %s %s %s %s %s %s -mask %s' % (os.path.join(mrtrix_bin_path, 'mtnormalise'), wm_fod, normalized_wm_fod,
+                                                 gm_fod, normalized_gm_fod, csf_fod, normalized_csf_fod, normalize_mask))

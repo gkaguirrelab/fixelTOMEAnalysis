85bf6759230305a69b4cd3399ab125528a971f28
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2021-02-05 15:11:05 -0500

    Subject FOD gear updated with some changes to the FSL path and mask fixes

--- fw_gears/createSubjectFODMap/create_fod_image.py
+++ fw_gears/createSubjectFODMap/create_fod_image.py
@@ -7,7 +7,16 @@ def create_fod_image(preprocessed_dti, mask, bvecs, bvals, response_wm, response
         os.system('mkdir %s' % workdir)
     if not os.path.exists(output_dir):
         os.system('mkdir %s' % output_dir)
-
+    
+    # Convert Pre-processed DTI to mif forma
+    if os.path.split(preprocessed_dti)[1][-7:] == '.nii.gz':
+        preprocessed_dti_mif = os.path.join(workdir, os.path.split(preprocessed_dti)[1].replace('.nii.gz', '.mif'))
+        os.system('%s %s %s' % os.path.join(mrtrix_bin_path, 'mrconvert'), preprocessed_dti, preprocessed_dti_mif)
+    if os.path.split(preprocessed_dti)[1][-4:] == '.mif':
+        preprocessed_dti_mif = preprocessed_dti
+    else:
+        raise RuntimeError('The input dti is not nifti or mif')
+    
     # Calculate 3-tissue functions for individual subjects
     if response_wm == '':
         print('Calculating 3-tissue response function')
@@ -15,7 +24,7 @@ def create_fod_image(preprocessed_dti, mask, bvecs, bvals, response_wm, response
         response_gm = os.path.join(workdir, 'response_gm.txt')    
         response_csf = os.path.join(workdir, 'response_csf.txt')    
         os.system('%s dhollander %s -fslgrad %s %s %s %s %s -scratch %s' % (os.path.join(mrtrix_bin_path, 'dwi2response'),
-                                                                                preprocessed_dti, bvecs, bvals, 
+                                                                                preprocessed_dti_mif, bvecs, bvals, 
                                                                                 response_wm, response_gm, response_csf, workdir))
     
     # Upsample DWI images and the mask to isotropic 1.25mm
@@ -23,7 +32,7 @@ def create_fod_image(preprocessed_dti, mask, bvecs, bvals, response_wm, response
     upsampled_dti = os.path.join(workdir, 'upsampled_%s_dti.mif' % subject_id)
     upsampled_mask = os.path.join(workdir, 'upsampled_%s_mask.mif' % subject_id)
     os.system('%s %s regrid -vox 1.25 %s' % (os.path.join(mrtrix_bin_path, 'mrgrid'),
-                                                    preprocessed_dti, upsampled_dti))
+                                                    preprocessed_dti_mif, upsampled_dti))
     os.system('%s %s regrid -vox 1.25 %s' % (os.path.join(mrtrix_bin_path, 'mrgrid'),
                                                     mask, upsampled_mask))
     
@@ -45,12 +54,21 @@ def create_fod_image(preprocessed_dti, mask, bvecs, bvals, response_wm, response
     # if a previous field correction was applied. It will coorect residual 
     # intensity inhomogeneities
     print('Correcting FOD intensity')
-    normalized_wm_fod = os.path.join(output_dir, 'preproc_wm_fod_%s.nii.gz' % subject_id)
-    normalized_gm_fod = os.path.join(output_dir, 'preproc_gm_fod_%s.nii.gz' % subject_id)
-    normalized_csf_fod = os.path.join(output_dir, 'preproc_csf_fod_%s.nii.gz' % subject_id)
+    normalized_wm_fod = os.path.join(output_dir, 'preproc_wm_fod_%s.mif' % subject_id)
+    normalized_gm_fod = os.path.join(output_dir, 'preproc_gm_fod_%s.mif' % subject_id)
+    normalized_csf_fod = os.path.join(output_dir, 'preproc_csf_fod_%s.mif' % subject_id)
     os.system('%s %s %s %s %s %s %s -mask %s' % (os.path.join(mrtrix_bin_path, 'mtnormalise'), wm_fod, normalized_wm_fod,
                                                  gm_fod, normalized_gm_fod, csf_fod, normalized_csf_fod, normalize_mask))
     
-    # Output the upsampled mask
-    mask_new_path = os.path.join(output_dir, 'mask_%s.nii.gz' % subject_id)
-    os.system('cp %s %s' % (upsampled_mask, mask_new_path))
+    # Save nifti versions of the images
+    normalized_wm_fod_nifti = os.path.join(output_dir, 'preproc_wm_fod_%s.nii.gz' % subject_id)
+    normalized_gm_fod_nifti = os.path.join(output_dir, 'preproc_gm_fod_%s.nii.gz' % subject_id)
+    normalized_csf_fod_nifti = os.path.join(output_dir, 'preproc_csf_fod_%s.nii.gz' % subject_id)
+    os.system('%s %s %s' % os.path.join(mrtrix_bin_path, 'mrconvert'), normalized_wm_fod, normalized_wm_fod_nifti)
+    os.system('%s %s %s' % os.path.join(mrtrix_bin_path, 'mrconvert'), normalized_gm_fod, normalized_gm_fod_nifti)
+    os.system('%s %s %s' % os.path.join(mrtrix_bin_path, 'mrconvert'), normalized_csf_fod, normalized_csf_fod_nifti)
+    
+    # Output the upsampled mask in mif and nifti formats
+    mask_new_path = os.path.join(output_dir, 'upsampled_%s_mask.nii.gz' % subject_id)    
+    os.system('cp %s %s' % (upsampled_mask, output_dir))
+    os.system('%s %s %s' % (os.path.join(mrtrix_bin_path, 'mrconvert'), upsampled_mask, mask_new_path))

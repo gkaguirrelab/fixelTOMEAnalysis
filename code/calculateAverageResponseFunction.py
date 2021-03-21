6663ae2c5a02a5c2474c93939858e8fcb3845e4f
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2021-03-21 04:30:52 -0400

    Response function gear for MRtrix FOD calculation finalized

--- code/calculateAverageResponseFunction.py
+++ code/calculateAverageResponseFunction.py
@@ -1,42 +1,44 @@
 import os 
 
-def calculateAverageResponseFunction(mrtrix_path, subject_mask_list, workdir, output_folder_path, shell='multi'):
+def calculateAverageResponseFunction(mrtrix_path, subject_dwi_list, bval_list, bvec_list, workdir, output_folder_path, shell='multi'):
     
     # Create folders in workdir for 3 responses
-    if shell == 'single':    
-        wm = os.path.join(workdir, 'wm')
+    wm = os.path.join(workdir, 'wm')
+    if not os.path.exists(wm):
         os.system('mkdir %s' % wm)   
-    elif shell == 'multi':
+    if shell == 'multi':
         gm = os.path.join(workdir, 'gm')
-        os.system('mkdir %s' % gm)        
+        if not os.path.exists(gm):
+            os.system('mkdir %s' % gm)        
         csf = os.path.join(workdir, 'csf')
-        os.system('mkdir %s' % csf)
-    else:
-        raise RuntimeError('shell type unrecognized use single or multi')
+        if not os.path.exists(csf):
+            os.system('mkdir %s' % csf)
    
     # Calculate response functions for each image
-    subject_name_initiate = '0'
-    for subject in subject_mask_list:
+    subject_name_initiate = 0
+    for subject in subject_dwi_list:
+        bval = bval_list[subject_name_initiate]
+        bvec = bvec_list[subject_name_initiate]
         if shell == 'single':
-            os.system('%s tournier %s %s' % (os.path.join(mrtrix_path, 'dwi2response'), subject,
-                                             os.path.join(wm, subject_name_initiate + '.txt')))
+            os.system('%s tournier %s -fslgrad %s %s %s' % (os.path.join(mrtrix_path, 'dwi2response'), subject,
+                                                            bvec, bval, os.path.join(wm, subject_name_initiate + '.txt')))
         elif shell == 'multi':                   
-            os.system('%s dhollander %s %s %s %s ' % (os.path.join(mrtrix_path, 'dwi2response'), subject,
-                                                      os.path.join(wm, subject_name_initiate + '.txt'),
-                                                      os.path.join(gm, subject_name_initiate + '.txt'),
-                                                      os.path.join(csf, subject_name_initiate + '.txt')))
+            os.system('%s dhollander %s -fslgrad %s %s %s %s %s ' % (os.path.join(mrtrix_path, 'dwi2response'), subject, bvec, bval,
+                                                                     os.path.join(wm, str(subject_name_initiate) + '.txt'),
+                                                                     os.path.join(gm, str(subject_name_initiate) + '.txt'),
+                                                                     os.path.join(csf, str(subject_name_initiate) + '.txt')))
+        subject_name_initiate += 1
 
-    # Average responses
-    if shell == 'single':  
-        group_average_wm = os.path.join(output_folder_path, 'group_average_wm.txt')
-        os.system('%s %s %s' % (os.path.join(mrtrix_path, 'reponsemean'),
+    # Average responses 
+    group_average_wm = os.path.join(output_folder_path, 'group_average_wm.txt')
+    os.system('%s %s %s' % (os.path.join(mrtrix_path, 'responsemean'),
                                 os.path.join(wm, '*'), group_average_wm))    
     if shell == 'multi':         
         group_average_gm = os.path.join(output_folder_path, 'group_average_gm.txt')
-        os.system('%s %s %s' % (os.path.join(mrtrix_path, 'reponsemean'),
+        os.system('%s %s %s' % (os.path.join(mrtrix_path, 'responsemean'),
                                 os.path.join(gm, '*'), group_average_gm))
         
         group_average_csf = os.path.join(output_folder_path, 'group_average_csf.txt')
-        os.system('%s %s %s' % (os.path.join(mrtrix_path, 'reponsemean'),
+        os.system('%s %s %s' % (os.path.join(mrtrix_path, 'responsemean'),
                                 os.path.join(csf, '*'), group_average_csf))
     

de274250f2528253b826bf93757eb0b86198a9ce
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2021-04-06 14:33:34 -0400

    Some corrections to extractdti

--- code/extract_dti_values.py
+++ code/extract_dti_values.py
@@ -1,12 +1,11 @@
-import os
+import os, subprocess
 
-def extract_dti_values(fa_image, other_metrics, workdir, output_folder_path, left_track, right_track, tckmap_template, mrtrix_path='', freesurfer_path='', ants_path='', track_density_thresh='1', binary_threshold='1'):
+def extract_dti_values(metric_image, workdir, output_folder_path, left_track, right_track, tckmap_template, mrtrix_path='', freesurfer_path='', ants_path='', fslpath='', track_density_thresh='1', binary_threshold='1'):
 
     '''
-    fa_image: input FA image from FSL DTIfit. FA image is used for registration since it has the best contrast among other
+    metric_image: input FA image from FSL DTIfit. FA image is used for registration since it has the best contrast among other
     DTI measures. Pass other metric images to other_metrics flag as a python list to move the tracks to 
     them.
-    other_metrics: other metrics to process
     workdir: Wordir folder where intermediate files will be saved
     output_folder_path: The output folder where the fixel files will be saved
     left_track: Left hemisphere track
@@ -18,7 +17,11 @@ def extract_dti_values(fa_image, other_metrics, workdir, output_folder_path, lef
     track_density_thresh: Select only those voxels that have the specified amount of fibres running through it
     binary_threshold: Threshold to binarize the track density images. 1 default is pretty liberal
     '''
-    #### Process tractography ####
+    ### Process tractography ####
+
+    # Get subject name and metric name
+    subject_id = os.path.split(metric_image)[1][:-10]
+    metric_name = os.path.split(metric_image)[1][-9:-7]
     
     # Convert vtk to tck
     tractography_folder = os.path.join(workdir, 'tractography')
@@ -42,65 +45,59 @@ def extract_dti_values(fa_image, other_metrics, workdir, output_folder_path, lef
         raise RuntimeError('You need to specify at least one track to extract values from')
     
     # Calculate track density map    
-    track_density_map = os.path.join(workdir, 'track_density_map.nii.gz')
+    track_density_map = os.path.join(tractography_folder, 'track_density_map.nii.gz')
     os.system('%s %s %s -template %s' % (os.path.join(mrtrix_path, 'tckmap'), left_and_right_tck, 
-                                         track_density_map, tckmap_template))
+                                          track_density_map, tckmap_template))
     
     # Threshold the track density map
-    thresholded_track = os.path.join(workdir, 'thresholded.nii.gz')
+    thresholded_track = os.path.join(tractography_folder, 'thresholded.nii.gz')
     os.system('%s %s -abs %s %s' % (os.path.join(mrtrix_path, 'mrthreshold'), track_density_map, 
                                     track_density_thresh, thresholded_track))
-    
-    # Binarize the thresholded track density map
-    binary_track_mask = os.path.join(workdir, 'binary_mask.nii.gz')
-    os.system('%s --i %s --min %s --o %s' % (os.path.join(freesurfer_path, 'mri_binarize'), thresholded_track,
-                                             binary_threshold, binary_track_mask))
     
     # Warp the image which the track was based on and warp it to the target    
     warp_workdir = os.path.join(workdir, 'warp_workdir')
     os.system('mkdir %s' % warp_workdir)
+    extracted_template = os.path.join(warp_workdir, 'extracted_template.nii.gz')
+    os.system('%s %s %s 0 1' % (os.path.join(fslpath,'fslroi'), tckmap_template, extracted_template))
     output_name = os.path.join(warp_workdir, 'tckmap2fod')
-    output_warped = os.path.join(warp_workdir, 'tckmap2fodWarped.nii.gz')
-    output_inverse_warped = os.path.join(warp_workdir, 'tckmap2fodInverseWarped.nii.gz')    
-    output_warp = os.path.join(warp_workdir, 'tckmap2fod1Warp.nii.gz')
-    output_affine = os.path.join(warp_workdir, 'tckmap2fod0GenericAffine.mat')
+    output_warped = os.path.join(warp_workdir, '%s_tckmap2fodWarped.nii.gz' % subject_id)
+    output_inverse_warped = os.path.join(warp_workdir, '%s_tckmap2fodInverseWarped.nii.gz' % subject_id)    
+    output_warp = os.path.join(warp_workdir, '%s_tckmap2fod1Warp.nii.gz' % subject_id)
+    output_affine = os.path.join(warp_workdir, '%s_tckmap2fod0GenericAffine.mat' % subject_id)
     
-    command = '%s --dimensionality 3 --float 0 --collapse-output-transforms 1 \
+    command = 'ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=7; %s --verbose 1 --dimensionality 3 --float 0 --collapse-output-transforms 1 \
 --output [ %s,%s,%s ] --interpolation Linear --use-histogram-matching 0 \
 --winsorize-image-intensities [ 0.005,0.995 ] --initial-moving-transform [ %s,%s,1 ] \
 --transform Rigid[ 0.1 ] --metric MI[ %s,%s,1,32,Regular,0.25 ] \
 --convergence [ 1000x500x250x100,1e-6,10 ] --shrink-factors 8x4x2x1 \
 --smoothing-sigmas 3x2x1x0vox --transform Affine[ 0.1 ] \
 --metric MI[ %s,%s,1,32,Regular,0.25 ] --convergence [ 1000x500x250x100,1e-6,10 ] \
+--shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox --transform SyN[ 0.1,3,0 ] \
+--metric CC[ %s,%s,1,4 ] --convergence [ 100x70x50x20,1e-6,10 ] \
 --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox' % (os.path.join(ants_path, 'antsRegistration'),
-                                                           output_name, output_warped, 
-                                                           output_inverse_warped, fa_image, 
-                                                           tckmap_template, fa_image, 
-                                                           tckmap_template, fa_image, 
-                                                           tckmap_template)
+                                                            output_name, output_warped, 
+                                                            output_inverse_warped, metric_image, 
+                                                            extracted_template, metric_image, 
+                                                            extracted_template, metric_image, 
+                                                            extracted_template, metric_image,
+                                                            extracted_template)
+    
     
     os.system(command)
     
     # Move the track to the target dti space
     binary_track_mask_warped = os.path.join(workdir, 'binary_mask_warped.nii.gz')
-    os.system('%s -d 3 -i %s -r %s -o %s -t %s -t %s' % (os.path.join(ants_path, 'applyApplyTransforms'),
-                                                         binary_track_mask, fa_image, binary_track_mask_warped,
-                                                         output_warp, output_affine))
+    os.system('%s -d 3 -i %s -r %s -o %s -n NearestNeighbor -t [ %s, 1 ] -t %s' % (os.path.join(ants_path, 'antsApplyTransforms'),
+                                                                                   thresholded_track, metric_image, binary_track_mask_warped,
+                                                                                   output_affine, output_inverse_warped))
     
-    # Extract the tracks from all metrics 
-    subject_id = os.path.split(fa_image)[1][:-10]
-    subject_folder = os.path.join(output_folder_path, subject_id)
-    FA_folder = os.path.join(subject_folder, 'FA')
-    os.system('mkdir %s' % FA_folder)
-    extracted_fa_track = os.path.join(FA_folder, 'FA.nii.gz')
+    # Extract the tracks from the metric
+    extracted_metric_image = os.path.join(workdir, '%s_%s_extracted.nii.gz' % (subject_id, metric_name))
     os.system('%s %s %s %s' % (os.path.join(freesurfer_path, 'mri_mask'),
-                               fa_image, binary_track_mask_warped, extracted_fa_track))    
-    for i in other_metrics:
-        metric_name = os.path.split(i)[1][-9:-7]
-        metric_folder = os.path.join(subject_folder, metric_name)
-        metric_file = os.path.join(metric_folder, metric_name + '.nii.gz')
-        os.system('%s %s %s %s' % (os.path.join(freesurfer_path, 'mri_mask'),
-                                   i, binary_track_mask_warped, metric_file))        
+                                metric_image, binary_track_mask_warped, extracted_metric_image))
     
+    # Get the mean metric value
+    metric_value_raw = subprocess.check_output(['%s' % os.path.join(fslpath, 'fslstats'), '%s' % extracted_metric_image, '-M'])
+    metric_value = float(metric_value_raw.decode('utf-8'))
     
-    
+    return metric_value

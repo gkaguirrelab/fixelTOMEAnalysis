565fc7831793cce1c3a119ab736beae6a00fc9e6
ozenc <HuseyinOzenc.Taskin@ pennmedicine.upenn.edu>
2021-08-31 12:52:36 -0400

    SS3T submit subjectFOD

--- submitGears/Single-shell-multi-tissue/2-ss3t-submit_create_subject_fod_map.py
+++ submitGears/Single-shell-multi-tissue/2-ss3t-submit_create_subject_fod_map.py
@@ -20,6 +20,7 @@ subject_list = ['TOME_3001','TOME_3002','TOME_3003','TOME_3004','TOME_3005',
                 'TOME_3039','TOME_3040','TOME_3042','TOME_3043','TOME_3044',
                 'TOME_3045','TOME_3046']
 
+# subject_list = ['TOME_3008']
 # Initialize gear stuff
 now = datetime.datetime.now().strftime("%y/%m/%d_%H:%M")
 fw = flywheel.Client()
@@ -43,8 +44,9 @@ config = {'convexOpt-BValHighTHD': '3500',
           'convexOpt-xi_NumSteps':'3',
           'convexOpt-xi_stepsize':'0.06',
           'method':'MRtrix sphericalDec',
-          'mrTrix-n_threads': '5',
-          'mrTrix-use_highest_b': True}
+          'mrTrix-n_threads': '6',
+          'mrTrix-use_highest_b': True,
+          'saveIntermediateFiles': True}
 
 # Get the response functions 
 response_gear = [ana for ana in analyses if ana.label.startswith('SS3T - calculateAverageResponseFunction')]
@@ -58,14 +60,15 @@ fails = []
 for analysis in hcp_results:
     ses = fw.get(analysis.parent.id)
     subject_name = ses.subject.code
-    input_file = analysis.get_file('%s_All_DTI_acqs_hcpdiff.zip' % subject_name)
-    
-    inputs = {'hcp_diff_archive': input_file, 'response_wm': wm, 'response_gm': gm, 'response_csf': csf}   
-    print('submitting createSubjectFODMap for %s' % subject_name)
-    try:
-        _id = qp.run(analysis_label=analysis_label,
-                      config=config, inputs=inputs, destination=ses)
-        analysis_ids.append(_id)
-    except Exception as e:
-        print(e)
-        fails.append(ses)
+    if subject_name in subject_list:
+        input_file = analysis.get_file('%s_All_DTI_acqs_hcpdiff.zip' % subject_name)
+        
+        inputs = {'hcp_diff_archive': input_file, 'response_wm': wm, 'response_gm': gm, 'response_csf': csf}   
+        print('submitting createSubjectFODMap for %s' % subject_name)
+        try:
+            _id = qp.run(analysis_label=analysis_label,
+                          config=config, inputs=inputs, destination=ses, tags=['vm-n1-highmem-8_disk-1500G_swap-60G'])
+            analysis_ids.append(_id)
+        except Exception as e:
+            print(e)
+            fails.append(ses)
